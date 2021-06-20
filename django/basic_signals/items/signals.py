eb890fe9e032fff0d75c6a46a6ed0e46703586ca
guido <guidomitolo@gmail.com>
2021-06-20 17:14:17 -0300

    more_signals

--- django/basic_signals/items/signals.py
+++ django/basic_signals/items/signals.py
@@ -7,23 +7,23 @@ import random
 # instance = the object/sender instanciated
 
 # pre save example
-def add_transcation_id(sender, instance, **kwargs):
-    if instance.trans_id == '':
-        instance.trans_id = str(random.randrange(0, 9999999999))
+def add_id_price(sender, instance, **kwargs):
+    instance.total_price = instance.stock_item.unit_price * instance.quantity
+    instance.trans_id = str(random.randrange(0, 9999999999))
 
 # post save example
 def update_stock(sender, instance, **kwargs):
     # update items from stock
     stock = Stock.objects.filter(id=instance.stock_item.id).first()
     stock.quantity = stock.quantity - instance.quantity
-    # contrary to pre_save it is mandatory to evoke .save method in post_save
     stock.save()
+
 
 # post save example
 def notify_user(sender, instance, **kwargs):
    print('Email sent to client!')
 
 
-pre_save.connect(add_transcation_id, sender=Order)
+pre_save.connect(add_id_price, sender=Order)
 post_save.connect(update_stock, sender=Order)
 post_save.connect(notify_user, sender=Order)
